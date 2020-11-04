84265c8da837518f8381abd8738de4d1cc9776ca
Kikiodazie <kiki.odazie@yahoo.com>
2020-11-04 16:43:05 +0100

    Initial Commit: Set-up

---
+++ src/main/java/com/odazie/rolebasedauthapi/RolebasedauthapiApplication.java
@@ -0,0 +1,13 @@
+package com.odazie.rolebasedauthapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RolebasedauthapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RolebasedauthapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/odazie/rolebasedauthapi/RolebasedauthapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odazie.rolebasedauthapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RolebasedauthapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
